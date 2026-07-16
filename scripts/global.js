e8ab5ad6b80b5b3d7d823f71913cea8e3d8d71a4
Michael Tumulak <mjtumulak.pea@gmail.com>
2026-07-16 12:25:37 +0800

    Fixed global js bug.

--- scripts/global.js
+++ scripts/global.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch('/pages/header.html')
+    fetch('./pages/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-placeholder').innerHTML = data;

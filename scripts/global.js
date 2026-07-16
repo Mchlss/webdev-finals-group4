4f1a18e1793e906098e249485236103673d45149
Michael Tumulak <mjtumulak.pea@gmail.com>
2026-07-16 13:58:37 +0800

    Fixed header bug

--- scripts/global.js
+++ scripts/global.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch('./pages/header.html')
+    fetch('/webdev-finals-group4/pages/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-placeholder').innerHTML = data;
